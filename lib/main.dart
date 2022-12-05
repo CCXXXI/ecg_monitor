50df9b4589805f9455dcdea556cce9687f5260f9
CCXXXI <ccxxxi@qq.com>
2022-12-06 06:26:12 +0800

    refactor: split home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,22 @@
 import "package:flutter/material.dart";
 
 import "constants.dart";
+import "home.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const App(),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: appName,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(appName),
-      ),
-      body: Center(child: Text("$_counter")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
-      ),
+      home: HomeView(),
     );
   }
 }
