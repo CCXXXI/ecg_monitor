44362c2b9b133d698d5478e55c741d76957f875c
CCXXXI <ccxxxi@qq.com>
2022-12-06 06:51:37 +0800

    refactor(home): refactor with riverpod

---
+++ lib/home.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// ignore_for_file: avoid_private_typedef_functions, non_constant_identifier_names, subtype_of_sealed_class, invalid_use_of_internal_member, unused_element, constant_identifier_names, unnecessary_raw_strings, library_private_types_in_public_api
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+String $CounterHash() => r'd7b735345f36157f0f650bfda814f0d2dc6cd647';
+
+/// See also [Counter].
+final counterProvider = AutoDisposeNotifierProvider<Counter, int>(
+  Counter.new,
+  name: r'counterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : $CounterHash,
+);
+
+typedef CounterRef = AutoDisposeNotifierProviderRef<int>;
+
+abstract class _$Counter extends AutoDisposeNotifier<int> {
+  @override
+  int build();
+}
