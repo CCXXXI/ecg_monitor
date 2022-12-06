e5bed700b8d20fa4c9724ad611bb47d060299527
CCXXXI <ccxxxi@qq.com>
2022-12-06 08:11:50 +0800

    feat(home): SalomonBottomBar

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,6 @@
-const String appName = "心电监测";
+class Strings {
+  static const String appName = "心电监测";
+  static const String monitor = "心电";
+  static const String device = "设备";
+  static const String mine = "我的";
+}
