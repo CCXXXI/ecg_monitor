61e4d2b23890caf87baa6e50c264ae60f6c7cd45
CCXXXI <ccxxxi@qq.com>
2022-12-06 16:51:20 +0800

    feat(analysis): AnalysisView

---
+++ lib/analysis.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class AnalysisView extends StatelessWidget {
+  const AnalysisView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/images/report placeholder.jpg");
+  }
+}
