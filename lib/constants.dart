396d3e8fc4859b8c78e5ae32d1b3506fca67282b
CCXXXI <ccxxxi@qq.com>
2023-01-31 03:45:22 +0800

    refactor(analytics): rename analysis to analytics

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 class Strings {
   static const String appName = "心电监测";
   static const String monitor = "实时数据";
-  static const String analysis = "分析报告";
+  static const String analytics = "分析报告";
   static const String device = "设备管理";
   static const String mine = "我的";
   static const String settings = "设置";
