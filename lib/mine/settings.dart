9e5bb1b5ef577df5f3a58cfde8a2aecd701cc92d
CCXXXI <ccxxxi@qq.com>
2023-01-31 06:43:25 +0800

    style: {super.key}

--- lib/mine/settings.dart
+++ lib/mine/settings.dart
@@ -3,7 +3,7 @@ import "package:flutter/material.dart";
 import "../constants.dart";
 
 class SettingsView extends StatelessWidget {
-  const SettingsView({Key? key}) : super(key: key);
+  const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
