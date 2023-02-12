7bf1c5a835764807fd6132fb317ced4072ada594
CCXXXI <ccxxxi@qq.com>
2023-02-12 23:43:55 +0800

    refactor(home): simplify index

--- lib/home.dart
+++ lib/home.dart
@@ -1,38 +1,30 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:go_router/go_router.dart";
-import "package:riverpod_annotation/riverpod_annotation.dart";
 
 import "utils/constants.dart";
-
-part "home.g.dart";
-
-@riverpod
-class _Index extends _$Index {
-  @override
-  int build() => 0;
-
-  void set(int index) => state = index;
-}
+import "utils/log.dart";
 
 class Home extends ConsumerWidget {
+  static const _routes = ["/monitor", "/analytics", "/device_manager", "/mine"];
+
   final Widget _child;
 
   const Home(this._child, {super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final index = ref.watch(_indexProvider);
+    final route = GoRouterState.of(context).location;
+    final index = _routes.indexOf(route);
+
+    logger.d("Home.build: route=$route, index=$index");
 
     return Scaffold(
       appBar: AppBar(title: const Text(Strings.appName)),
       body: _child,
       bottomNavigationBar: NavigationBar(
         selectedIndex: index,
-        onDestinationSelected: (i) {
-          ref.read(_indexProvider.notifier).set(i);
-          context.go(["/monitor", "/analytics", "/device_manager", "/mine"][i]);
-        },
+        onDestinationSelected: (i) => context.go(_routes[i]),
         destinations: const [
           NavigationDestination(
             icon: Icon(Icons.monitor_heart),
