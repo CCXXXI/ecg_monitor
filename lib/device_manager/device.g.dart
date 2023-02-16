7255665fed3e90cc8740dcab38a655043a06625b
CCXXXI <ccxxxi@qq.com>
2023-02-16 11:47:31 +0800

    chore: add build_runner files to git
    [SEVERE] riverpod_generator on lib/mine/settings.dart:
    
    Bad state: Failed to obtain the unit: too many inconsistent analysis exceptions.
    
    随机出现，概率不高，目前无彻底解决方案，提交到 Git 似乎是个可行的 Workaround

---
+++ lib/device_manager/device.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'device.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$deviceHash() => r'ea3792c80cf0a6028118bd5bb0dffe4d80304bac';
+
+/// See also [device].
+@ProviderFor(device)
+final deviceProvider = AutoDisposeProvider<Device?>.internal(
+  device,
+  name: r'deviceProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$deviceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef DeviceRef = AutoDisposeProviderRef<Device?>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
