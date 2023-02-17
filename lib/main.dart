0f492311c6d7b5ca8748a9bf96f7c28f24994a16
CCXXXI <ccxxxi@qq.com>
2023-02-18 04:18:55 +0800

    feat: init sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:sentry_flutter/sentry_flutter.dart";
 
 import "analytics/model_stub.dart" if (dart.library.io) "analytics/model.dart";
 import "database.dart";
@@ -15,8 +16,14 @@ void main() async {
   initLogger();
   await loadModel();
 
-  // run app
-  runApp(const ProviderScope(child: App()));
+  // init Sentry & run app
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = Strings.sentryDsn;
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(const ProviderScope(child: App())),
+  );
 }
 
 class App extends StatelessWidget {
