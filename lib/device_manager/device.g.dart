cb83867eb136ccb914ed625257d1e6a0bb331812
CCXXXI <ccxxxi@qq.com>
2023-02-18 22:06:24 +0800

    chore: rerun build_runner

--- lib/device_manager/device.g.dart
+++ lib/device_manager/device.g.dart
@@ -6,7 +6,7 @@ part of 'device.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$currentDeviceHash() => r'9d04aa9fe11c1d6e9bd3d2568c897a46d0f1fcee';
+String _$currentDeviceHash() => r'81b3201032b043d466a05c15ea4a2faae3a7b34f';
 
 /// See also [CurrentDevice].
 @ProviderFor(CurrentDevice)
