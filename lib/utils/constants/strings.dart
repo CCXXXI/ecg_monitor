5023d3dae2ce5c3ed8373589f0d3af365d01f03f
CCXXXI <ccxxxi@qq.com>
2023-02-19 02:00:26 +0800

    feat(monitor): setting group

--- lib/utils/constants/strings.dart
+++ lib/utils/constants/strings.dart
@@ -4,6 +4,7 @@ const monitor = "实时数据";
 const style = "样式";
 const professional = "专业";
 const simple = "简洁";
+const custom = "自定义";
 const portraitDuration = "竖屏时长";
 const landscapeDuration = "横屏时长";
 const recent = "最近";
