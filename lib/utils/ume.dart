570993bda803edf3911332c94233699255c4b4bf
CCXXXI <ccxxxi@qq.com>
2023-03-15 14:41:08 +0800

    feat(ume): add DioInspector

--- lib/utils/ume.dart
+++ lib/utils/ume.dart
@@ -2,12 +2,14 @@ import "package:flutter/foundation.dart";
 import "package:flutter_ume/flutter_ume.dart";
 import "package:flutter_ume_kit_console/flutter_ume_kit_console.dart";
 import "package:flutter_ume_kit_device/flutter_ume_kit_device.dart";
+import "package:flutter_ume_kit_dio/flutter_ume_kit_dio.dart";
 import "package:flutter_ume_kit_perf/flutter_ume_kit_perf.dart";
 import "package:flutter_ume_kit_show_code/flutter_ume_kit_show_code.dart";
 import "package:flutter_ume_kit_ui/flutter_ume_kit_ui.dart"; // UI 插件包
 
 import "../database.dart";
 import "constants/keys.dart" as key;
+import "dio.dart";
 
 /// Plugins that are always available.
 final _pluginsAlwaysOn = <Pluggable>[
@@ -17,6 +19,7 @@ final _pluginsAlwaysOn = <Pluggable>[
   Console(),
   CpuInfoPage(),
   const DeviceInfoPanel(),
+  DioInspector(dio: dio),
   Performance(),
 ];
 
