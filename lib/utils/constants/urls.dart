e58634fee35eee68c6d132b0c173986ac1d67ba1
CCXXXI <ccxxxi@qq.com>
2023-03-15 20:00:38 +0800

    feat(settings): update test url

--- lib/utils/constants/urls.dart
+++ lib/utils/constants/urls.dart
@@ -4,4 +4,4 @@ const _blob = "$_repo/blob/main";
 final issues = Uri.parse("$_repo/issues");
 final changelog = Uri.parse("$_blob/CHANGELOG.md#changelog");
 
-final test = Uri.parse("https://www.bing.com");
+final test = Uri.parse("https://mock.codes/200");
