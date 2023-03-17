5be1281d91931ff5453cfbaf22a32c3a70d4ff07
CCXXXI <ccxxxi@qq.com>
2023-03-18 00:43:39 +0800

    fix(home): import foundation

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:functional_widget_annotation/functional_widget_annotation.dart";
