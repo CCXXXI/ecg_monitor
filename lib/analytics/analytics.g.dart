687483c8462aa3c09595ef7534b2d4c0a1da8a0b
CCXXXI <ccxxxi@qq.com>
2023-03-17 10:08:51 +0800

    refactor(analytics): make Analytics swidget

---
+++ lib/analytics/analytics.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'analytics.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class Analytics extends StatelessWidget {
+  const Analytics({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => analytics();
+}
