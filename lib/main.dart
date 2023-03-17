3a1f804f2f758d9e3dd635c90f4f7f0591b9e1a1
CCXXXI <ccxxxi@qq.com>
2023-03-18 00:32:45 +0800

    refactor(main): make App swidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:flutter_ume/flutter_ume.dart";
+import "package:functional_widget_annotation/functional_widget_annotation.dart";
 import "package:sentry_flutter/sentry_flutter.dart";
 import "package:sentry_logging/sentry_logging.dart";
 
@@ -13,6 +14,8 @@ import "utils/license.dart";
 import "utils/logger.dart";
 import "utils/router.dart";
 import "utils/ume.dart";
+
+part "main.g.dart";
 
 void main() async {
   // initializations
@@ -43,19 +46,15 @@ void main() async {
 
 /// The root widget of the app.
 /// Wrap [AppCore] with multiple Widgets to provide additional functionality.
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) => SentryScreenshotWidget(
-        child: UMEWidget(
-          enable: prefs.getBool(key.showDevTools) ?? false,
-          child: const ProviderScope(
-            child: AppCore(),
-          ),
+@swidget
+Widget _app(BuildContext context) => SentryScreenshotWidget(
+      child: UMEWidget(
+        enable: prefs.getBool(key.showDevTools) ?? false,
+        child: const ProviderScope(
+          child: AppCore(),
         ),
-      );
-}
+      ),
+    );
 
 /// The core widget of the app.
 /// With no additional functionality for testing purposes.
---
+++ lib/main.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+/// The root widget of the app.
+/// Wrap [AppCore] with multiple Widgets to provide additional functionality.
+class App extends StatelessWidget {
+  /// The root widget of the app.
+  /// Wrap [AppCore] with multiple Widgets to provide additional functionality.
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => _app(_context);
+}
