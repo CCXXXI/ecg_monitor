55b38cb4505cb62f7fe95e2a299e53c0aa21be40
CCXXXI <ccxxxi@qq.com>
2023-03-20 06:25:40 +0800

    fix: rerun build_runner
    flutter pub run build_runner build --delete-conflicting-outputs

--- lib/device_manager/device.g.dart
+++ lib/device_manager/device.g.dart
@@ -6,7 +6,7 @@ part of 'device.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$currentDeviceHash() => r'48b9b8da0607f1ba8a353e555edc590497a67887';
+String _$currentDeviceHash() => r'7703f30584c43d9e1723f63fb0159d7d89aaa179';
 
 /// See also [CurrentDevice].
 @ProviderFor(CurrentDevice)
