725e80e42a1cb72ce8d6a5d317faf6154b2dd0b2
CCXXXI <ccxxxi@qq.com>
2023-03-22 14:49:55 +0800

    feat(analytics): read counts from database

--- lib/analytics/analytics.dart
+++ lib/analytics/analytics.dart
@@ -1,13 +1,15 @@
 import "package:flutter/material.dart";
 import "package:functional_widget_annotation/functional_widget_annotation.dart";
+import "package:isar/isar.dart";
 
 import "../generated/l10n.dart";
+import "../utils/database.dart";
 import "data_types.dart";
 
 part "analytics.g.dart";
 
-// todo: read from database
-int _labelCount(Label label) => 0;
+int _labelCount(Label label) =>
+    isar.beats.where().labelEqualTo(label).countSync();
 
 @swidget
 Widget analytics(BuildContext context) {
@@ -37,8 +39,9 @@ Widget analytics(BuildContext context) {
       ),
       ListTile(
         title: Text(s.paroxysmalSupraVentricularTachycardia),
-        trailing: Text(_labelCount(Label.paroxysmalSupraVentricularTachycardia)
-            .toString()),
+        trailing: Text(
+          _labelCount(Label.paroxysmalSupraVentricularTachycardia).toString(),
+        ),
       ),
       ListTile(
         title: Text(s.ventricularPreExcitation),
@@ -47,7 +50,8 @@ Widget analytics(BuildContext context) {
       ListTile(
         title: Text(s.ventricularFlutterAndFibrillation),
         trailing: Text(
-            _labelCount(Label.ventricularFlutterAndFibrillation).toString()),
+          _labelCount(Label.ventricularFlutterAndFibrillation).toString(),
+        ),
       ),
       ListTile(
         title: Text(s.atrioventricularBlock),
