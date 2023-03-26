37d819b10690d16149ecaa190c1b4ca1fc6ec405
CCXXXI <ccxxxi@qq.com>
2023-03-26 17:39:31 +0800

    refactor(database): add logs

--- lib/utils/database.dart
+++ lib/utils/database.dart
@@ -1,11 +1,14 @@
 import "package:flutter/material.dart";
 import "package:isar/isar.dart";
+import "package:logging/logging.dart";
 import "package:shared_preferences/shared_preferences.dart";
 
 import "../analytics/data_types.dart";
 import "../device_manager/device.dart";
 
 part "database.g.dart";
+
+final _logger = Logger("Database");
 
 @visibleForTesting
 @collection
@@ -67,22 +70,36 @@ Future<void> initDatabase() async {
 }
 
 // region Beat
-Future<void> writeBeatData(BeatData data) => _isar.writeTxn(
-      () async => _isar.beats.put(Beat.fromBeatData(data)),
-    );
+void writeBeatData(BeatData data) {
+  final stopwatch = Stopwatch()..start();
+  _isar.writeTxnSync(() => _isar.beats.putSync(Beat.fromBeatData(data)));
+  _logger.finer("Writing beat data took ${stopwatch.elapsed}.");
+}
 
-int labelCount(Label label) =>
-    _isar.beats.where().labelEqualTo(label).countSync();
+int labelCount(Label label) {
+  final stopwatch = Stopwatch()..start();
+  final count = _isar.beats.where().labelEqualTo(label).countSync();
+  _logger.fine("Counting label $label took ${stopwatch.elapsed}.");
 
-List<DateTime> labelTimes(Label label) => _isar.beats
-    .where()
-    .labelEqualTo(label)
-    .millisecondsSinceEpochProperty()
-    .findAllSync()
-    .map(DateTime.fromMillisecondsSinceEpoch)
-    .toList(growable: false);
+  return count;
+}
+
+List<DateTime> labelTimes(Label label) {
+  final stopwatch = Stopwatch()..start();
+  final msSinceEpoch = _isar.beats
+      .where()
+      .labelEqualTo(label)
+      .millisecondsSinceEpochProperty()
+      .findAllSync();
+  _logger.fine("Finding label $label took ${stopwatch.elapsed}.");
+
+  return msSinceEpoch
+      .map(DateTime.fromMillisecondsSinceEpoch)
+      .toList(growable: false);
+}
 
 List<BeatData> beatDataBetween(DateTime start, DateTime end) {
+  final stopwatch = Stopwatch()..start();
   final data = _isar.beats
       .where()
       .millisecondsSinceEpochBetween(
@@ -90,17 +107,24 @@ List<BeatData> beatDataBetween(DateTime start, DateTime end) {
         end.millisecondsSinceEpoch,
       )
       .findAllSync();
+  _logger.fine("Finding beats between $start and $end "
+      "took ${stopwatch.elapsed}.");
 
   return data.map((d) => d.toBeatData()).toList(growable: false);
 }
 // endregion
 
 // region SamplePoint
-Future<void> writeEcgData(EcgData data) => _isar.writeTxn(
-      () async => _isar.samplePoints.put(SamplePoint.fromEcgData(data)),
-    );
+void writeEcgData(EcgData data) {
+  final stopwatch = Stopwatch()..start();
+  _isar.writeTxnSync(
+    () => _isar.samplePoints.putSync(SamplePoint.fromEcgData(data)),
+  );
+  _logger.finest("Writing sample point took ${stopwatch.elapsed}.");
+}
 
 List<EcgData> ecgDataBetween(DateTime start, DateTime end) {
+  final stopwatch = Stopwatch()..start();
   final data = _isar.samplePoints
       .where()
       .millisecondsSinceEpochBetween(
@@ -108,6 +132,8 @@ List<EcgData> ecgDataBetween(DateTime start, DateTime end) {
         end.millisecondsSinceEpoch,
       )
       .findAllSync();
+  _logger.fine("Finding sample points between $start and $end "
+      "took ${stopwatch.elapsed}.");
 
   return data.map((d) => d.toEcgData()).toList(growable: false);
 }
