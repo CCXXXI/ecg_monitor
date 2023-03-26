bffbcc624f611320351c96ed034a4faae4214ba6
CCXXXI <ccxxxi@qq.com>
2023-03-26 12:30:03 +0800

    test(chart): add test for Chart3Lead

--- test/ecg/chart_test.dart
+++ test/ecg/chart_test.dart
@@ -1,4 +1,8 @@
 import "package:ecg_monitor/ecg/chart.dart";
+import "package:ecg_monitor/generated/l10n.dart";
+import "package:ecg_monitor/me/settings/data_types.dart";
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:flutter_test/flutter_test.dart";
 import "package:quiver/time.dart";
 
@@ -27,5 +31,33 @@ void main() {
         expect(getInterval(aSecond * i, isPortrait: false), aSecond * 2);
       }
     });
+  });
+
+  testWidgets("Chart3Lead", (tester) async {
+    final s = await S.load(S.delegate.supportedLocales.first);
+
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          localizationsDelegates: [S.delegate],
+          home: Chart3Lead(
+            pointsI: [],
+            pointsII: [],
+            pointsIII: [],
+            duration: aSecond,
+            backgroundColor: Colors.white,
+            lineColor: Colors.black,
+            gridColor: Colors.black,
+            horizontalLineType: LineType.hide,
+            verticalLineType: LineType.hide,
+            showDots: false,
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text(s.leadI), findsOneWidget);
+    expect(find.text(s.leadII), findsOneWidget);
+    expect(find.text(s.leadIII), findsOneWidget);
   });
 }
