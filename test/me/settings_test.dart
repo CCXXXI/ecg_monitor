b0ae380d5fb7962bbd3caaeb225d4f54f6c228c7
CCXXXI <ccxxxi@qq.com>
2023-03-26 14:55:30 +0800

    feat(settings): improve duration pickers

--- test/me/settings_test.dart
+++ test/me/settings_test.dart
@@ -3,9 +3,9 @@ import "package:flutter_test/flutter_test.dart";
 import "package:quiver/time.dart";
 
 void main() {
-  group("DurationToSecondsString", () {
-    test("1.0 s", () => expect(aSecond.toSecondsString(), "1.0 s"));
-    test("1.5 s", () => expect((aSecond * 1.5).toSecondsString(), "1.5 s"));
-    test("2.0 s", () => expect((aSecond * 2).toSecondsString(), "2.0 s"));
+  group("Duration.toMsString", () {
+    test("1000 ms", () => expect(aSecond.toMsString(), "1000 ms"));
+    test("1500 ms", () => expect((aSecond * 1.5).toMsString(), "1500 ms"));
+    test("2000 ms", () => expect((aSecond * 2).toMsString(), "2000 ms"));
   });
 }
